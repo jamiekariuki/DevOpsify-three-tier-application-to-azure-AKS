9acffbab995c253f5ad4a319cf036ee7047b368a
kar!s <jamiekariuki18@gmail.com>
2025-07-05 02:03:22 +0300

    initial commit

---
+++ backend/src/main/java/com/sergio/crud/backend/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.sergio.crud.backend.config;
+
+import org.springframework.beans.factory.annotation.Value; 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Value("${cors.allowed-origin}")
+    private String allowedOrigin;
+
+    @Bean
+    public WebMvcConfigurer corsConfig() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                        .allowedOrigins(allowedOrigin);
+            }
+        };
+    }
+}
